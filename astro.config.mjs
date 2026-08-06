6e26b3ab4dfa57a5d5138eb536aaf258ca874857
trai <frenchpeopleamright@gmail.com>
2026-08-06 13:53:56 +1000

    fix: update base URL asset resolution for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://KinsBandOfficial.github.io',
+  base: process.env.GITHUB_ACTIONS ? '/kins-link-in-bio/' : '/',
   build: {
     format: 'directory'
   }
